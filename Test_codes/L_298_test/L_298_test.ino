021dee335590262dce8928684f759d68573c139d
Jeon JUNI <jjh63360@kau.kr>
2021-11-15 16:32:05 +0900

    Add motor driver test Code

---
+++ Test_codes/L_298_test/L_298_test.ino
@@ -0,0 +1,21 @@
+const int enable_pinA = 4;
+const int motorA_inputA = 18;
+const int motorA_inputB = 19; 
+
+void setup(){
+  ledcAttachPin(enable_pinA, 0);
+  pinMode(motorA_inputA, OUTPUT);
+  pinMode(motorA_inputB, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(motorA_inputA, HIGH);
+  digitalWrite(motorA_inputB, LOW);
+  delay(2000);
+
+  digitalWrite(motorA_inputA, LOW);
+  digitalWrite(motorA_inputB, HIGH);
+  delay(2000);
+
+  
+}
