e21381a95be36d309063de9db9671f5318f9c9b6
전준휘 <jjh63360@kau.kr>
2021-12-17 22:22:45 +0900

    FIX: 서보 각 수정

--- Test_codes/servo_test/servo_test.ino
+++ Test_codes/servo_test/servo_test.ino
@@ -7,13 +7,11 @@ Servo servo1;
 void setup() {
     Serial.begin(115200);
     servo1.attach(servoPin); // D27에 서보모터가 연결되었습니다.
-    servo1.write(2);
-    delay(2000);
 }
 
 void loop() {
    servo1.write(120);
-   delay(10000);
+   delay(1000);
    servo1.write(0);
-   delay(10000);
+   delay(1000);
 }
